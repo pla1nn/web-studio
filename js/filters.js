2ee9f67befb947df69e53120a717f48064aafc85
pla1n <114383720+pla1nn@users.noreply.github.com>
2023-08-21 19:12:03 +0300

    update

---
+++ js/filters.js
@@ -0,0 +1,15 @@
+const filterCard = document.querySelectorAll('.card')
+
+document.querySelector('.projects-nav-list').addEventListener('click', event => {
+    if (event.target.tagName !== 'projects-button') return false;
+
+    let filterClass = event.target.dataset['f'];
+    
+    filterBox.forEach(elem =>{
+        elem.classList.remove('hide')
+        if (!elem.classList.contains(filterClass) && filterClass!== 'all') {
+            elem.classList.add('hide');
+        }
+    })
+
+});
